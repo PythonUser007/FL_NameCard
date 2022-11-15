fa6f209d7e9839289ed0dbcba409d8afb30de45f
PythonUser007 <mypythonemailid@gmail.com>
2022-11-15 22:30:14 +0530

    Version 4 for my Name Card. Updated the main.py. Changed the application name to app

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-MyNameCard = Flask(__name__)
+app = Flask(__name__)
 
-@MyNameCard.route("/")
+@app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    MyNameCard.run(debug=True)
+    app.run(debug=True)
